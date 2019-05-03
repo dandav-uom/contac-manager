48a73a50f59f572b9ca20c2f71aeea4521995549
dandav-uom <daniel.davies-5@postgrad.manchester.ac.uk>
2019-05-03 23:14:40 +0100

    Updates

---
+++ precache-manifest.03fff3a580ace5227a3c05a02b840a9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62938c87a0c0a4ed1ce9bd39053e847e",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "435e4e1936bdbe47700c",
+    "url": "/contact-manager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ec03daae19abe19f2d4e",
+    "url": "/contact-manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "435e4e1936bdbe47700c",
+    "url": "/contact-manager/static/js/2.41cea7ef.chunk.js"
+  },
+  {
+    "revision": "ec03daae19abe19f2d4e",
+    "url": "/contact-manager/static/js/main.a10750da.chunk.js"
+  },
+  {
+    "revision": "a25dc548b2853493d42e",
+    "url": "/contact-manager/static/js/runtime~main.b61356a3.js"
+  }
+]);
